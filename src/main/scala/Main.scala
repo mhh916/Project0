7cfde2e9c49d6867dc660a8dba60e64028b305f1
Michael Hayes <michaelhayes22222@gmail.com>
2021-10-08 17:39:44 -0400

    Created basic command line loop

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,58 @@
-object Main extends App {
-  println("Hello, World!")
-  println("This is to test git")
+
+import scala.io.StdIn._
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Welcome to Venom Corporation's Hotel Management System")
+    var manager = readLine("Insert Username: ")
+    println()
+
+    var loop = true
+    println(s"Welcome $manager, please select an option")
+    do{
+      println("1. Check-in\n2. Check-out\n3. Charge Client\n4. View Guest List\n5. Log-Out\n6. Quit Application")
+      val option = readInt()
+      option match{
+        case 1 => {
+          println("Not yet implemented")
+          println()
+          Thread.sleep(1000)
+        }
+
+        case 2 => {
+          println("Not yet implemented")
+          println()
+          Thread.sleep(1000)
+        }
+
+        case 3 => {
+          println("Not yet implemented")
+          println()
+          Thread.sleep(1000)
+        }
+
+        case 4 => {
+          println("Not yet implemented")
+          println()
+          Thread.sleep(1000)
+        }
+
+        case 5 => {
+          println("Logout Successfull")
+          println()
+          Thread.sleep(1000)
+          println("Welcome to Venom Corporation's Hotel Management System")
+          manager = readLine("Insert Username: ")
+          println()
+          println(s"Welcome $manager, please select an option")
+          
+        }  
+        
+        case 6 => loop = false
+        
+      }
+    } while(loop) 
+    println("Thank you for using Venom Corporation's Hotel Management System")
+    Thread.sleep(2000)
+  }
+  
 }
